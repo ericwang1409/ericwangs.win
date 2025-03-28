3206ce49a7a4bc4cdd1cb22b1c24186750046d31
Eric <eric3691215@gmail.com>
2025-03-28 14:42:26 -0700

    template

--- .next/static/chunks/app_layout_tsx_f0e4c1a2._.js
+++ .next/static/chunks/app_layout_tsx_f0e4c1a2._.js
@@ -5,7 +5,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/app_layout_tsx_f0e4c1a2._.js",
   "chunks": [
-    "static/chunks/[root of the server]__98144f0b._.css"
+    "static/chunks/_a1b7b2f6._.css",
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_56b44225._.js",
+    "static/chunks/node_modules_96f5620d._.js"
   ],
   "source": "dynamic"
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-import Image from "next/image";
+import { BlogPosts } from "./components/posts";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="flex h-screen justify-center items-center">
-      <div className="text-center bg-blue-400">
-        <h1 className="text-3xl">HEADING</h1>
-        <p className="text-xl">Sub text</p>
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Eric Wang
+      </h1>
+      <p className="mb-4">
+        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
+        Vim's keystroke commands and tabs' flexibility for personal viewing
+        preferences. This extends to my support for static typing, where its
+        early error detection ensures cleaner code, and my preference for dark
+        mode, which eases long coding sessions by reducing eye strain.`}
+      </p>
+      <div className="my-8">
+        <BlogPosts />
       </div>
-    </div>
+    </section>
   );
 }
