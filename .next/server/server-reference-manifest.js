0ae7c8d668e2f69070745646574aedc005363004
Eric <eric3691215@gmail.com>
2025-03-28 14:32:05 -0700

    init

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"T9vmPnI6wxu4c3W9U66q0Bu+rThOOIjQs6gAnIMQ+N0=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="text-center bg-blue-400">
+        <h1 className="text-3xl">HEADING</h1>
+        <p className="text-xl">Sub text</p>
+      </div>
+    </div>
+  );
+}
