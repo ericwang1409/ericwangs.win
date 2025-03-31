842de8def04abca526315bd8003fb16839eba416
Eric <eric3691215@gmail.com>
2025-03-31 14:50:41 -0700

    readings page with search bar

--- app/readings/page.tsx
+++ app/readings/page.tsx
@@ -4,8 +4,18 @@ import Fuse from "fuse.js";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { useEffect } from "react";
+import { CiSearch } from "react-icons/ci";
 
-const originalLinks = ["article 1", "article 2", "banana", "apple"];
+const originalLinks = new Map([
+  [
+    "The AI Revolution (Part 1)",
+    "https://waitbutwhy.com/2015/01/artificial-intelligence-revolution-1.html",
+  ],
+  [
+    "A defense of slowness at the end of the world",
+    "https://longerramblings.substack.com/p/a-defence-of-slowness-at-the-end",
+  ],
+]);
 
 export default function Page() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -15,7 +25,7 @@ export default function Page() {
     const options = {
       includeScore: false,
     };
-    const fuse = new Fuse(originalLinks, options);
+    const fuse = new Fuse(Array.from(originalLinks.keys()), options);
     setLinks(fuse.search(searchTerm).map((result) => result.item));
   }, [searchTerm]);
 
@@ -27,24 +37,39 @@ export default function Page() {
           Posts, articles, books I&apos;ve found interesting
         </h2>
       </div>
-      <Input
-        placeholder="Search"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+      <div className="relative w-full">
+        <Input
+          type="search"
+          placeholder="Search"
+          value={searchTerm}
+          className="pl-8 rounded-md border border-gray-100 shadow-sm focus:ring-1 focus:ring-gray-900 focus:border-gray-900"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <CiSearch className="absolute left-2.5 top-2.5 h-4 w-4 pointer-events-none opacity-50" />
+      </div>
       <div>
         {(links && links.length > 0) || searchTerm.length > 0
           ? links.map((link, index) => (
               <div key={index} className="my-4">
-                <a href={link} className="text-blue-500">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={link}
+                  className="hover:text-yellow-500"
+                >
                   {link}
                 </a>
               </div>
             ))
-          : originalLinks.map((link, index) => (
+          : Array.from(originalLinks.entries()).map(([title, url], index) => (
               <div key={index} className="my-4">
-                <a href={link} className="text-blue-500">
-                  {link}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={url}
+                  className="hover:text-yellow-500"
+                >
+                  {title}
                 </a>
               </div>
             ))}
