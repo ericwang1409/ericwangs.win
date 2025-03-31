c78cdc24a2a7fbce9f70efd73634bd412e03556a
Eric <eric3691215@gmail.com>
2025-03-30 23:24:45 -0700

    searching

--- app/readings/page.tsx
+++ app/readings/page.tsx
@@ -1,10 +1,23 @@
+"use client";
+
 import Fuse from "fuse.js";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { useEffect } from "react";
 
+const originalLinks = ["article 1", "article 2", "banana", "apple"];
+
 export default function Page() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [links, setLinks] = useState([""]);
+
+  useEffect(() => {
+    const options = {
+      includeScore: false,
+    };
+    const fuse = new Fuse(originalLinks, options);
+    setLinks(fuse.search(searchTerm).map((result) => result.item));
+  }, [searchTerm]);
 
   return (
     <section>
@@ -14,7 +27,28 @@ export default function Page() {
           Posts, articles, books I&apos;ve found interesting
         </h2>
       </div>
-      <Input placeholder="Search" value={searchTerm} />
+      <Input
+        placeholder="Search"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div>
+        {(links && links.length > 0) || searchTerm.length > 0
+          ? links.map((link, index) => (
+              <div key={index} className="my-4">
+                <a href={link} className="text-blue-500">
+                  {link}
+                </a>
+              </div>
+            ))
+          : originalLinks.map((link, index) => (
+              <div key={index} className="my-4">
+                <a href={link} className="text-blue-500">
+                  {link}
+                </a>
+              </div>
+            ))}
+      </div>
     </section>
   );
 }
